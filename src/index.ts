d7e1927146e9077772ec2e8116ed789906865043
Mohammed Usama <usamabhakrani@gmail.com>
2024-01-18 15:11:36 +0500

    Code Reuse with Interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const numbersCollection = new NumbersCollection([10, 3, 1, 55, -55, 0]);
+const numbersCollection = new NumbersCollection([10, 3, 1, 55, -55, 0,50]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.length);
